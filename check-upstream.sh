b34afeef51d948e2af0892c5b6188d4befe6d19b
Tim Gymnich <tgymnich@icloud.com>
2022-02-09 17:22:20 +0100

    Create check-upstream.sh

---
+++ check-upstream.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git -C /home/ubuntu/Enzyme fetch
+HEADHASH=$(git -C /home/ubuntu/Enzyme rev-parse HEAD)
+UPSTREAMHASH=$(git -C /home/ubuntu/Enzyme rev-parse main@{upstream})
+
+if [ "$HEADHASH" != "$UPSTREAMHASH" ]
+  then
+    bash /home/ubuntu/build.sh 
+    exit 0
+  else
+    echo -e ${FINISHED}Current branch is up to date with origin/master.${NOCOLOR}
+fi
