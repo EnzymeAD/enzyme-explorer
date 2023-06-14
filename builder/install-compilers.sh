2e2be8f8971d686bc17514b4022eaf90214389ab
Tim Gymnich <tgymnich@icloud.com>
2023-06-15 00:42:27 +0200

    simplify install scripts

--- builder/install-compilers.sh
+++ builder/install-compilers.sh
@@ -2,54 +2,5 @@
 
 set +e
 
-# clang
+/app/infra/bin/ce_install --filter-match-any install "compilers/c++/clang 9.0.1" "compilers/c++/clang 10.0.1" "compilers/c++/clang 11.0.1" "compilers/c++/clang 12.0.1" "compilers/c++/clang 13.0.1" "compilers/c++/clang 14.0.0" "compilers/c++/clang 15.0.0" "compilers/c++/clang 16.0.0" "compilers/c++/mlir 16.0.0" "compilers/julia 1.8.5"
 
-if /app/infra/bin/ce_install check-installed compilers/c++/clang 9.0.1 | grep "not installed"; then
-    /app/infra/bin/ce_install install compilers/c++/clang 9.0.1
-fi
-
-if /app/infra/bin/ce_install check-installed compilers/c++/clang 10.0.1 | grep "not installed"; then
-    /app/infra/bin/ce_install install compilers/c++/clang 10.0.1
-fi
-
-if /app/infra/bin/ce_install check-installed compilers/c++/clang 11.0.1 | grep "not installed"; then
-    /app/infra/bin/ce_install install compilers/c++/clang 11.0.1
-fi
-
-if /app/infra/bin/ce_install check-installed compilers/c++/clang 12.0.1 | grep "not installed"; then
-    /app/infra/bin/ce_install install compilers/c++/clang 12.0.1
-fi
-
-if /app/infra/bin/ce_install check-installed compilers/c++/clang 13.0.1 | grep "not installed"; then
-    /app/infra/bin/ce_install install compilers/c++/clang 13.0.1
-fi
-
-if /app/infra/bin/ce_install check-installed compilers/c++/clang 14.0.0 | grep "not installed"; then
-    /app/infra/bin/ce_install install compilers/c++/clang 14.0.0
-fi
-
-if /app/infra/bin/ce_install check-installed compilers/c++/clang 15.0.0 | grep "not installed"; then
-    /app/infra/bin/ce_install install compilers/c++/clang 15.0.0
-fi
-
-if /app/infra/bin/ce_install check-installed compilers/c++/clang 16.0.0 | grep "not installed"; then
-    /app/infra/bin/ce_install install compilers/c++/clang 16.0.0
-fi
-
-# mlir
-
-if /app/infra/bin/ce_install check-installed compilers/c++/mlir 16.0.0 | grep "not installed"; then
-    /app/infra/bin/ce_install install compilers/c++/mlir 16.0.0
-fi
-
-# cuda
-
-# if /app/infra/bin/ce_install check-installed compilers/cuda 11.0.2 | grep "not installed"; then
-#     /app/infra/bin/ce_install install compilers/cuda 11.0.2
-# fi
-
-# julia
-
-if /app/infra/bin/ce_install check-installed compilers/julia 1.8.5 | grep "not installed"; then
-    /app/infra/bin/ce_install install compilers/julia 1.8.5
-fi
--- builder/install-libraries.sh
+++ builder/install-libraries.sh
@@ -2,17 +2,7 @@
 
 set +e
 
-if /app/infra/bin/ce_install check-installed libraries/c++/boost 1.81.0 | grep "not installed"; then
-    /app/infra/bin/ce_install install libraries/c++/boost 1.81.0
-fi
-
-if /app/infra/bin/ce_install check-installed libraries/c++/nlohmann_json 3.11.1 | grep "not installed"; then
-    /app/infra/bin/ce_install install libraries/c++/nlohmann_json 3.11.1
-fi
-
-if /app/infra/bin/ce_install check-installed libraries/c++/eigen 3.4.0 | grep "not installed"; then
-    /app/infra/bin/ce_install install libraries/c++/eigen 3.4.0
-fi
+/app/infra/bin/ce_install --filter-match-any install "libraries/c++/boost 1.82.0" "libraries/c++/nlohmann_json 3.11.1" "libraries/c++/eigen 3.4.0"
 
 export JULIA_DEPOT_PATH="/opt/compiler-explorer/juliapackages"
 /opt/compiler-explorer/julia-1.8.5/bin/julia -e 'using Pkg; Pkg.add("Enzyme")'
