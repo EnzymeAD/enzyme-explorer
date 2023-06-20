bbe9cbe73df10b0355912abd0518b57b342b64b8
Tim Gymnich <tgymnich@icloud.com>
2023-06-21 01:34:02 +0200

    remove +e

--- builder/install-libraries.sh
+++ builder/install-libraries.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set +e
-
 /app/infra/bin/ce_install --filter-match-any install "libraries/c++/boost 1.82.0" "libraries/c++/nlohmann_json 3.11.1" "libraries/c++/eigen 3.4.0"
 
 export JULIA_DEPOT_PATH="/opt/compiler-explorer/juliapackages"
