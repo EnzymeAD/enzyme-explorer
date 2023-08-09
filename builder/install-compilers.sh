d57c25333bd6ea70cb921fcc05cf3f000c835e1f
Tim Gymnich <tgymnich@icloud.com>
2023-08-09 17:08:29 +0200

    install cuda

--- builder/install-compilers.sh
+++ builder/install-compilers.sh
@@ -3,3 +3,5 @@
 /app/infra/bin/ce_install --filter-match-any install "compilers/c++/clang 9.0.1" "compilers/c++/clang 10.0.1" "compilers/c++/clang 11.0.1" "compilers/c++/clang 12.0.1" "compilers/c++/clang 13.0.1" "compilers/c++/clang 14.0.0" "compilers/c++/clang 15.0.0" "compilers/c++/clang 16.0.0" "compilers/c++/mlir 16.0.0" "compilers/julia 1.8.5"
 
 /app/infra/bin/ce_install --enable nightly install "compilers/c++/nightly/clang assertions-trunk"
+
+/app/infra/bin/ce_install "compilers/cuda 12.1.0" 
