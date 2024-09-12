82b9fc2c64d12c58d17d73b5fab6371d4596d381
Tim Gymnich <tgymnich@icloud.com>
2024-09-12 13:52:52 +0200

    drop julia 1.8 and 1.9

--- builder/install-libraries.sh
+++ builder/install-libraries.sh
@@ -3,8 +3,6 @@
 /app/infra/bin/ce_install --filter-match-any install "libraries/c++/boost 1.82.0" "libraries/c++/nlohmann_json 3.11.1" "libraries/c++/eigen 3.4.0"
 
 export JULIA_DEPOT_PATH="/opt/compiler-explorer/juliapackages"
-/opt/compiler-explorer/julia-1.8.5/bin/julia -e 'using Pkg; Pkg.add("Enzyme")'
-/opt/compiler-explorer/julia-1.9.3/bin/julia -e 'using Pkg; Pkg.add("Enzyme")'
 /opt/compiler-explorer/julia-1.10.0/bin/julia -e 'using Pkg; Pkg.add("Enzyme")'
 
 curl -O https://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.19.4.tar.gz
