6f73fd02cbc3ef4b181951f20e5a58b42b0bc8d4
Tim Gymnich <tgymnich@icloud.com>
2024-09-27 23:42:52 +0200

    add rust

--- builder/install-compilers.sh
+++ builder/install-compilers.sh
@@ -5,3 +5,8 @@
 /app/infra/bin/ce_install --enable nightly install "compilers/c++/nightly/clang assertions-trunk"
 
 /app/infra/bin/ce_install install "compilers/cuda 11.8.0" 
+
+curl -O https://github.com/EnzymeAD/rust/releases/download/enzyme-0.0.1/rustc-nightly-x86_64-unknown-linux-gnu.tar.gz 
+mkdir -p /opt/compiler-explorer/rust-nightly-enzyme
+tar -C /opt/compiler-explorer/rust-nightly-enzyme -xf rustc-nightly-x86_64-unknown-linux-gnu.tar.gz
+rm rustc-nightly-x86_64-unknown-linux-gnu.tar.gz
