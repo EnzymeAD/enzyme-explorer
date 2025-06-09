bf33fabc1582741985e802b2c387a2c14de82bba
Tim Gymnich <tgymnich@icloud.com>
2025-06-09 12:56:34 +0200

    avoid confirmation prompt

--- update.sh
+++ update.sh
@@ -6,4 +6,4 @@ docker service update --force --image ghcr.io/enzymead/enzyme-explorer-builder:l
 docker service update --force --image ghcr.io/enzymead/enzyme-explorer:latest enzyme_explorer_compiler-explorer
 docker service update enzyme_explorer_package-proxy
 
-docker system prune
+docker system prune -f
